cb2f7ef7ffeffefdc82b1ead93c126aeeffc3be6
Andres27Gallardo <andres.gallardo@ucb.edu.bo>
2024-09-11 19:25:34 -0400

    ACT.11.SEP.24

--- MES 1/CALCULADORA.CONVERSION.py
+++ MES 1/CALCULADORA.CONVERSION.py
@@ -4,6 +4,9 @@ def menuDeSeleccion():
     print("1. Metros a Kilomegros")
     print("2. Gramos a kilometros")
     print("3. Celsius a Fahrenheit")
+    print("4. Kilometro a metro")
+    print("5. Kilogramo a gramo")
+    print("6. Fahrenheir a celsius")
     
 print("-----Calculadora de conversion------ ")
 
@@ -17,8 +20,12 @@ elif opcion == 2:
     print(f"Resultado: {conversiones.GramosAKilogramos(cantidad)}")
 elif opcion == 3:
     print(f"Resultado: {conversiones.CelsiusAFahrenheit(cantidad)}")
-else:
-    print("No existe esta opción")
+elif opcion == 4:
+    print(f"Resultado: {conversiones.kilometroAMetro(cantidad)}")
+elif opcion == 5:
+    print(f"Resultado: {conversiones.KilogramoAGramo(cantidad)}")
+elif opcion == 6:
+    print(f"Resultado: {conversiones.FahrenheitACelsius(cantidad)}")
         
 
     
--- MES 1/conversiones.py
+++ MES 1/conversiones.py
@@ -7,3 +7,11 @@ def GramosAKilogramos(cantidad):
 def CelsiusAFahrenheit(cantidad):
     return (cantidad*1.8)+32
 
+def kilometroAMetro(cantidad):
+    return cantidad*1000
+
+def KilogramoAGramo(cantidad):
+    return cantidad*1000
+
+def FahrenheitACelsius(cantidad):
+    return (cantidad-32)*1.8
