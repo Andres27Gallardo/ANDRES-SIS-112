f31a477d7732cbd438d5a19fc482be05c1046999
Andres27Gallardo <andres.gallardo@ucb.edu.bo>
2024-09-13 18:19:59 -0400

    ACT.13.SEP.24

---
+++ MES 1/ADIVINA.NUMERO.py
@@ -0,0 +1,22 @@
+import random
+print("Bienvenido a ADIVINA EL NUMERO")
+print("El numero buscado es de 0 al 10")
+
+numeroAle = random.randint(0,10)
+
+contador = 0
+
+while True:
+    numeroUsu= int(input("Seleccione un numero: "))
+    if numeroUsu < numeroAle:
+        print("Demasiado bajo, intente de nuevo")
+    elif numeroUsu > numeroAle:
+        print("Demasiado alto, intente de nuevo")
+    elif numeroUsu == numeroAle:
+        print("---------------")
+        print("¡¡¡¡¡Felicidades!!!!")
+        break
+    contador = contador+1
+
+print("El numero de intentos es: ",contador)
+print("----------------")
